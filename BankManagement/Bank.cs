635795e7dddd1afb50a138c8347505f09037836c
TIWARI Mohit <mohit.tiwari@soprasteria.com>
2022-10-14 14:30:02 +0530

    initial commit

---
+++ BankManagement/Bank.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagement
+{
+    internal class Bank
+    {
+        string Name, DOB, FatherName, Aadhar, Address;
+        int InitialAmount = 0;
+        int sub = 0, sum = 0;
+        static int AccountNo = 1;
+        static int ATMBalance;
+        public static void SetATM()
+        {
+            ATMBalance = 500000;
+        }
+        public Bank()
+        {
+            Console.WriteLine(" Enter Your Name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine(" Enter Your DOB: ");
+            DOB = Console.ReadLine();
+            Console.WriteLine(" Enter Your Father's Name: ");
+            FatherName = Console.ReadLine();
+            Console.WriteLine(" Enter Your Address: ");
+            Address = Console.ReadLine();
+            Console.WriteLine(" Enter Your AadharNo: ");
+            Aadhar = Console.ReadLine();
+            Console.WriteLine(" Enter Initial Amount : ");
+            InitialAmount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Account has been Created. ");
+            Console.Write("Dear Coustomer " + Name + "Your Account No is ");
+            Console.WriteLine(AccountNo);
+            AccountNo++;
+            Console.WriteLine("Your Initial Amount is " + InitialAmount);
+        }
+        public void WithDraw()
+        {
+            Console.WriteLine("Enter the Amount: ");
+            sub = Convert.ToInt32(Console.ReadLine());
+            if (InitialAmount > sub)
+            {
+                InitialAmount = InitialAmount - sub;
+                ATMBalance = ATMBalance - sub;
+                Console.WriteLine(" Dear " + Name + "Your Balance Amount is " + InitialAmount);
+                Console.WriteLine("ATM Balance : " + ATMBalance);
+            }
+            else
+            {
+                Console.WriteLine(" Your Balance is Low. ");
+            }
+        }
+        public void Deposite()
+        {
+            Console.WriteLine("Enter the Amount: ");
+            sum = Convert.ToInt32(Console.ReadLine());
+            InitialAmount = InitialAmount + sum;
+            Console.WriteLine(" Dear " + Name + " Your Balance After Deposit is " + InitialAmount);
+
+        }
+    }
+}
---
+++ BankManagement/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bank> Customer = new List<Bank>();
+            string ch;
+            int Choice, Press;
+            do
+            {
+                Console.WriteLine("Do You want to Create New Account : Y/N");
+                ch = Console.ReadLine();
+                if (ch.Equals("Y"))
+                {
+                    Bank obj = new Bank(); // Constructor Has Been called
+                    do
+                    {
+                        Console.WriteLine("1.WithDraw ");
+                        Console.WriteLine("2.Deposit ");
+                        Console.WriteLine("3.Exit ");
+                        Console.WriteLine("Enter Your Choice");
+                        Choice = Convert.ToInt32(Console.ReadLine());
+                        if (Choice == 1)
+                        {
+                            obj.WithDraw();
+                        }
+                        else if (Choice == 2)
+                        {
+                            obj.Deposite();
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine(" Do you want to continue : \n Press: 1 for Continue \n Press 0 for Exit");
+                        Press = Convert.ToInt32(Console.ReadLine());
+                    } while (Press == 1);
+
+                    Customer.Add(obj);
+                }
+                else
+                {
+                    Console.WriteLine("Enter a Valid Choice ");
+                }
+            } while (ch.Equals("Y"));
+        }
+    }
+}
